56f54f14e567cf0e3e8d55135b1e62cc1a0420d4
jewis1 <jehad.jde@gmail.com>
2026-09-25 12:10:28 -0400

    Associate failed logins with usernames

--- analyzer.py
+++ analyzer.py
@@ -9,7 +9,10 @@ failed_count = 0
 with open("sample_logs/login.log", "r") as log_file:
   for line in log_file:
     if "LOGIN_FAILED" in line:
+      parts = line.split()
+      username = parts[3]
       failed_count = failed_count + 1
+      print("Failed login user:", username)
       check_login("FAILED")
    
     elif "LOGIN_SUCCESS" in line:
